b833542642368c8f3e2776d5fd1e03faeadfe592
cktang88 <arctangent20@yahoo.com>
2018-01-29 16:46:41 -0600

    Prettify all code

--- Block.js
+++ Block.js
@@ -1,43 +1,43 @@
 //class for individual blocks
 class Block {
-    //note to self: required variables go first
-    constructor(data) {
-      this.index = 0;
-      this.timestamp = new Date();
-      this.data = data;
-      this.previousHash = "";
-      this.nonceVal = 0;
-      this.hash = this.calculateHash();
-    }
-  
-    //get a hash code for every new block that we have
-    calculateHash() {
-      return keccak(
-        this.index +
-          this.timestamp +
-          this.previousHash +
-          JSON.stringify(this.data) +
-          this.nonceVal
-      );
-    }
-  
-    mineBlock(difficulty) {
-      let beginningZeros = "";
-      for (let i = 0; i < difficulty; i++) {
-        beginningZeros += "0";
-      }
-  
-      while (this.hash.substring(0, difficulty) !== beginningZeros) {
-        this.nonceVal = randSafeInt();
-        this.hash = this.calculateHash();
-      }
-  
-      if (this.index !== 0) {
-        console.log(`Block ${this.index} mined! Hashcode: ${this.hash}`);
-      } else {
-        console.log("Genesis block created!");
-      }
-    }
+  //note to self: required variables go first
+  constructor(data) {
+    this.index = 0;
+    this.timestamp = new Date();
+    this.data = data;
+    this.previousHash = "";
+    this.nonceVal = 0;
+    this.hash = this.calculateHash();
   }
 
-  module.exports = Block;
+  //get a hash code for every new block that we have
+  calculateHash() {
+    return keccak(
+      this.index +
+        this.timestamp +
+        this.previousHash +
+        JSON.stringify(this.data) +
+        this.nonceVal
+    );
+  }
+
+  mineBlock(difficulty) {
+    let beginningZeros = "";
+    for (let i = 0; i < difficulty; i++) {
+      beginningZeros += "0";
+    }
+
+    while (this.hash.substring(0, difficulty) !== beginningZeros) {
+      this.nonceVal = randSafeInt();
+      this.hash = this.calculateHash();
+    }
+
+    if (this.index !== 0) {
+      console.log(`Block ${this.index} mined! Hashcode: ${this.hash}`);
+    } else {
+      console.log("Genesis block created!");
+    }
+  }
+}
+
+module.exports = Block;
