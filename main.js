ddcec334b4d5305f46cad4b77b0c819b6514a718
cktang88 <arctangent20@yahoo.com>
2018-01-29 16:45:58 -0600

    Fix repo name

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const keccak = text =>
 const randSafeInt = () => randomInt(Number.MAX_SAFE_INTEGER);
 const isAffirmative = text => ["y", "yes"].includes(text.toLowerCase());
 
-console.log("Welcome to Makeshift Blockchain Maker!");
+console.log("Welcome to Blockchain Builder!");
 console.log("Creating new Blockchain...");
 let name = readline.question("Set Blockchain name: ");
 let difficulty = readline.question("Set level of difficulty: ");
